3b7785924ff66d4904ec6edd3c2174c3a4852eff
Renan Martini <renan@Mac-mini-de-Renan.local>
2023-11-16 12:59:44 -0300

    feat(healthcheck): created healthcheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './resources/products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { HealthController } from './app.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { ImportHistoryRepository } from './resources/products/repository/importHistory.repository';
+import { HistorySchema } from './resources/products/schemas/history.entity';
+import { HealthService } from './app.service';
 
 @Module({
   imports: [
@@ -10,8 +15,10 @@ import { ConfigModule } from '@nestjs/config';
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@cluster0.ku3maxu.mongodb.net/?retryWrites=true&w=majority`,
     ),
+    MongooseModule.forFeature([{ name: 'history', schema: HistorySchema }]),
+    TerminusModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [HealthController],
+  providers: [ImportHistoryRepository, HealthService],
 })
 export class AppModule {}
